d6c900f9b1b92a45661aa70f577bac8f88738a1a
furkansimsekli <simseklifurkan0@gmail.com>
2024-05-06 21:53:38 +0300

    member: adapt headers to new csv format

--- member.py
+++ member.py
@@ -10,15 +10,15 @@ class Member:
 
     @classmethod
     def parse_list(cls, l: list[str]):
-        [enroll_date, name, student_id, department, degree, email, mobile_number, elo, lichess_username,
-         chesscom_username, note, is_added_to_group] = l
+        [enroll_date, name, surname, mobile_number, student_id, department, degree,
+         ukd, elo, is_paid, is_added_to_group] = l
 
         if is_added_to_group == "1" or is_added_to_group == "-1":
             return None
 
         note = f"{student_id} {department} {degree}"
 
-        name = f"HUSAT {name.strip().title()}"
+        name = f"HUSAT {name.strip().title()} {surname.strip().title()}"
 
         telno = mobile_number.replace(" ", "")
         if telno.startswith("5"):
@@ -28,7 +28,7 @@ class Member:
         elif telno.startswith("9"):
             telno = "+" + telno
 
-        return cls(name, telno, email, note)
+        return cls(name, telno, "", note)
 
     def __repr__(self) -> str:
         return f"'{self.name}','{self.telno}','{self.email}','{self.note}'\n"
